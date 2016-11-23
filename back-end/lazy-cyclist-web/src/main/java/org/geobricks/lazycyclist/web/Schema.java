f8692437206f624228b845d664ce2aeaf12455c3
Guido Barbaglia <guido.barbaglia@rea-group.com>
2016-11-23 13:50:03 +1100

    Merge development in.

---
+++ back-end/lazy-cyclist-web/src/main/java/org/geobricks/lazycyclist/web/Schema.java
@@ -0,0 +1,42 @@
+package org.geobricks.lazycyclist.web;
+
+import javax.ws.rs.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class Schema {
+
+  private String schema;
+  private String schemaPath;
+
+  public Schema(Resource schemaPath) throws Exception {
+    try                   { this.setSchemaPath(schemaPath.getFile().getPath()); }
+    catch (IOException e) { throw new Exception(e.getMessage()); }
+  }
+
+  @GET
+  public Response schema() {
+    return Response.status(200).entity(this.schema).build();
+  }
+
+  public void init() throws IOException {
+    String path = this.schemaPath + File.separator + "schema.json";
+    byte[] encoded = Files.readAllBytes(Paths.get(path));
+    this.setSchema(new String(encoded, Charset.defaultCharset()));
+  }
+
+  private void setSchemaPath(String schemaPath) { this.schemaPath = schemaPath; }
+  private void setSchema(String schema)         { this.schema = schema; }
+}
