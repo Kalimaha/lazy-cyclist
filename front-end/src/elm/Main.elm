b5e625ab6ae133dc97ebc3b02a6ede08019ed49b
Guido Barbaglia <guido.barbaglia@rea-group.com>
2016-11-24 12:19:06 +1100

    Page skeleton.

--- front-end/src/elm/Main.elm
+++ front-end/src/elm/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, div, text, program, h1, form, label, input, br)
+import Html.Attributes exposing (..)
 
 type alias Model =
   String
@@ -14,8 +15,25 @@ type Msg =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ text model ]
+  div [ class "row" ] [
+    div [ class "col-lg-12" ] [
+      Html.form [] [
+        div [ class "form-group" ] [
+          label [ for "from" ] [ text "From" ]
+          , input [
+              class "form-control"
+              , placeholder "e.g. Federation Square, Melbourne, Australia"
+            ] []
+          , br [] []
+          , label [ for "to" ] [ text "To" ]
+          , input [
+              class "form-control"
+              , placeholder "e.g. 511 Church St, Melbourne, Australia"
+            ] []
+        ]
+      ]
+    ]
+  ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
