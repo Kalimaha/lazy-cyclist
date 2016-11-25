55a6ff4170d917073ecbe6e4b80190cd382ff4b3
Guido Barbaglia <guido.barbaglia@rea-group.com>
2016-11-25 15:14:02 +1100

    1st working version.

--- front-end/src/elm/Main.elm
+++ front-end/src/elm/Main.elm
@@ -1,10 +1,11 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Http
 import Json.Decode as Decode
+import Json.Encode as Encode
 
 type alias Model = {
   from: String,
@@ -29,42 +30,52 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-  div [ class "row" ] [
-    div [ class "col-lg-12" ] [
-      div [ class "form-group" ] [
-        label [] [
-          i [ class "fa fa-home" ] []
-          , text " From"
-        ]
-        , input [
-            class "form-control input-lg"
-            , type_ "text"
-            , placeholder "e.g. Federation Square, Melbourne, Australia"
-            , onInput From
-            , value model.from
-          ] []
-        , br [] []
-        , label [] [
-          i [ class "fa fa-map-marker" ] []
-          , text " To"
-        ]
-        , input [
-            class "form-control input-lg"
-            , type_ "text"
-            , placeholder "e.g. 511 Church St, Melbourne, Australia"
-            , onInput To
-            , value model.to
-          ] []
-        , br [] []
-        , button [
-          class "btn btn-lg btn-primary"
-          , onClick Submit
-          ] [
-          i [ class "fa fa-bicycle" ] []
-          , text " Route!"
+  div [] [
+    div [ class "row" ] [
+      div [ class "col-lg-12" ] [
+        div [ class "form-group" ] [
+          label [] [
+            i [ class "fa fa-home" ] []
+            , text " From"
+          ]
+          , input [
+              class "form-control input-lg"
+              , type_ "text"
+              , placeholder "e.g. Federation Square, Melbourne, Australia"
+              , onInput From
+              , value model.from
+            ] []
+          , br [] []
+          , label [] [
+            i [ class "fa fa-map-marker" ] []
+            , text " To"
+          ]
+          , input [
+              class "form-control input-lg"
+              , type_ "text"
+              , placeholder "e.g. 511 Church St, Melbourne, Australia"
+              , onInput To
+              , value model.to
+            ] []
+          , br [] []
+          , button [
+            class "btn btn-lg btn-primary"
+            , onClick Submit
+            ] [
+            i [ class "fa fa-bicycle" ] []
+            , text " Route!"
+          ]
         ]
       ]
     ]
+    , hr [] []
+    , div [ class "row" ] [
+        div [ class "col-lg-12" ] [
+          div [ id "chart_0" ] [ text "Chart 1 Here" ]
+          , div [ id "chart_1" ] [ text "Chart 1 Here" ]
+          , div [ id "chart_2" ] [ text "Chart 1 Here" ]
+        ]
+      ]
   ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -77,13 +88,34 @@ update msg model =
     Submit ->
       (model, routes model)
     Routes (Ok body) ->
-      Debug.log "OK"
-      Debug.log (toString body)
-      (model, Cmd.none)
+      (model, highcharts (List.indexedMap toChart body))
     Routes (Err e) ->
-      Debug.log "ERROR"
-      Debug.log (toString e)
       (model, Cmd.none)
+
+toChart : Int -> ElevationProfile -> Chart
+toChart idx ep =
+  ({
+    id = "chart_" ++ toString idx,
+    payload = chartPayload ep
+  })
+
+chartPayload : ElevationProfile -> String
+chartPayload ep =
+  Debug.log (toString ep.points)
+  -- Debug.log (toString encodePoints ep.points)
+  """{"series":[{"data":""" ++ (Encode.encode 0 (encodePoints ep.points)) ++ """}]}"""
+
+encodePoint : Point -> Encode.Value
+encodePoint point =
+  Encode.list [Encode.float point.x, Encode.float point.y]
+
+encodePoints : List Point -> Encode.Value
+encodePoints points =
+  Encode.list (List.map encodePoint points)
+
+  -- toString (List.map (\p -> "[" ++ (toString p.x) ++ "," ++ (toString p.y) ++ "]") points)
+
+port highcharts : (List Chart) -> Cmd msg
 
 routes : Model -> Cmd Msg
 routes model =
@@ -98,6 +130,11 @@ routes model =
 decodeRoutes : Decode.Decoder String
 decodeRoutes =
   Decode.string
+
+type alias Chart = {
+  id: String,
+  payload: String
+}
 
 type alias ElevationProfile = {
   totalDistance: Float,
