f8e02bec3c4ff861eb65346ad9d3a38f3506d3c6
Guido Barbaglia <guido.barbaglia@rea-group.com>
2017-02-21 16:06:00 +1100

    Setup Redux and tests.

--- front-end/redux/src/index.js
+++ front-end/redux/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import App from './js/components/app'
 import Reducer from './js/reducers/index'
+import thunkMiddleware from 'redux-thunk'
 
 
-const store = createStore(Reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  Reducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+)
 const placeholder = document.getElementById('placeholder')
 
 render(
--- front-end/redux/src/js/components/form.js
+++ front-end/redux/src/js/components/form.js
@@ -33,6 +33,19 @@ const Form = ({ from, to, updateFrom, updateTo, submitAction, valid }) => (
     <br />
     <div className="row">
       <div className="col-lg-12">
+        <label>
+          Extra Elevation Points
+        </label>
+      </div>
+      <div className="col-lg-12">
+      <label className="radio-inline"><input type="radio" name="optradio" checked=""/>0</label>
+      <label className="radio-inline"><input type="radio" name="optradio" checked="checked"/>1</label>
+      <label className="radio-inline"><input type="radio" name="optradio" checked=""/>2</label>
+      </div>
+    </div>
+    <br />
+    <div className="row">
+      <div className="col-lg-12">
         <button disabled={!valid} onClick={submitAction} className="btn btn-primary" style={{width: "100%"}}>
           Route!
         </button>
