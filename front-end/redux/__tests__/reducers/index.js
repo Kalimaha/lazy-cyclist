6c478f65ff0937935aeca0d64dd4a2a162c19ad0
Guido Barbaglia <guido.barbaglia@rea-group.com>
2017-02-21 16:38:09 +1100

    Reducer tests.

---
+++ front-end/redux/__tests__/reducers/index.js
@@ -0,0 +1,50 @@
+import Reducer from '../../src/js/reducers/index'
+
+describe('Reducer', () => {
+  const from          = 'Initial Location'
+  const to            = 'Final Location'
+  const initial_state = {
+    from:     from,
+    to:       to,
+    valid:    true,
+    message:  'Valid selection.'
+  }
+
+  it('returns the initial state', () => {
+    expect(Reducer(undefined, {})).toEqual(initial_state)
+  })
+
+  it('handles the UPDATE_FROM action', () => {
+    expect(Reducer(initial_state, { type: 'UPDATE_FROM', from: 'a' })).toEqual({
+      from:     'a',
+      to:       to,
+      valid:    true,
+      message:  'Valid selection.'
+    })
+  })
+
+  it('handles the UPDATE_TO action', () => {
+    expect(Reducer(initial_state, { type: 'UPDATE_TO', to: 'b' })).toEqual({
+      from:     from,
+      to:       'b',
+      valid:    true,
+      message:  'Valid selection.'
+    })
+  })
+
+  it('provides a message when the selection is invalid', () => {
+    expect(Reducer(initial_state, { type: 'UPDATE_FROM', from: '' })).toEqual({
+      from:     '',
+      to:       to,
+      valid:    false,
+      message:  'Both "From" and "To" fields are required.'
+    })
+
+    expect(Reducer(initial_state, { type: 'UPDATE_TO', to: '' })).toEqual({
+      from:     from,
+      to:       '',
+      valid:    false,
+      message:  'Both "From" and "To" fields are required.'
+    })
+  })
+})
